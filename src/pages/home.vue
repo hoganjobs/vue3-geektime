6b45bc0e1003392fef1b4d3e683e43658861a7d9
Hogan <hongju110@126.com>
2022-06-27 14:19:43 +0800

    useMouse

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>home page</h1>
   <TodoList />
+  <div>x:{{ x }} | y:{{ y }}</div>
 </template>
 <script setup>
 import TodoList from '../components/TodoList.vue';
+import { useMouse } from '../utils/mouse';
+
+let { x, y } = useMouse()
 </script>
