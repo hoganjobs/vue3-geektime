78abe3007774ee1efb0657d5ea6cb7b2277fb3ca
Hogan <hongju110@126.com>
2022-06-27 14:28:56 +0800

    style bind. use  style variable 绑定color变量

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,9 +2,11 @@
   <h1>home page</h1>
   <TodoList />
   <div>x:{{ x }} | y:{{ y }}</div>
+  <StyleBind />
 </template>
 <script setup>
 import TodoList from '../components/TodoList.vue';
+import StyleBind from '../components/StyleBind.vue';
 import { useMouse } from '../utils/mouse';
 
 let { x, y } = useMouse()
