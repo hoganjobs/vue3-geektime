a3dfbf01bc30e26d7a06e22cd5910a3a172d1cd9
Hogan <hongju110@126.com>
2022-06-27 15:04:09 +0800

    favicon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
   <template>
+  <button @click="loading">123</button>
   <div>
-    <router-link to="/">首页</router-link> | 
+    <router-link to="/">首页</router-link> |
     <router-link to="/about">关于</router-link>
   </div>
   <router-view></router-view>
 </template>
+<script setup>
+import useFavicon from './utils/favicon'
+let { favicon } = useFavicon()
+function loading() {
+  favicon.value = '/geek.png'
+}
+</script>
