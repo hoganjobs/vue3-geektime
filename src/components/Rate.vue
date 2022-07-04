280972cda7ee9ae6b012eea0ef9198c687167977
Hogan <hongju110@126.com>
2022-07-04 11:34:26 +0800

    rate emits

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -1,9 +1,16 @@
 <template>
-  <div :style="fontStyle">{{ rate }}</div>
+  <div :style="fontStyle">
+    <div class="rate" @mouseout="mouseOut">
+      <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+      <span class="hollow" :style="fontwidth">
+        <span @click="onRate(num)" @mouseover="mouseOver(num)" v-for="num in 5" :key="num">★</span>
+      </span>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { defineProps, computed } from 'vue'
+import { defineProps, computed, ref, defineEmits } from 'vue'
 
 let props = defineProps({
   value: Number,
@@ -23,4 +30,34 @@ const themeObj = {
 let fontStyle = computed(() => {
   return `color: ${themeObj[props.theme]}`
 })
+
+let width = ref(props.value)
+function mouseOut() {
+  width.value = props.value
+}
+function mouseOver(i) {
+  width.value = i
+}
+const fontwidth = computed(() => `width: ${width.value}em;`)
+
+let emits = defineEmits('update-rate')
+function onRate(num) {
+  emits('update-rate', num)
+}
 </script>
+
+<style scoped>
+.rate {
+  position: relative;
+  display: inline-block;
+}
+
+.rate>span.hollow {
+  position: absolute;
+  display: inline-block;
+  top: 0;
+  left: 0;
+  width: 0;
+  overflow: hidden;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,13 @@
 <template>
   <h1>home page</h1>
-  <Rate :value="3" />
-  <Rate :value="4" theme="red" />
-  <Rate :value="1" theme="green" />
+  <h3>you rate: {{score}}</h3>
+  <Rate :value="score" @updateRate="update" />
 </template>
 <script setup>
+import { ref } from 'vue';
 import Rate from '../components/Rate.vue';
+let score = ref(3.5)
+function update(num) {
+  score.value = num
+}
 </script>
