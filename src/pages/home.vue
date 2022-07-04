ec269fa63ef2657c5ea9ecb7a804172f9035d3d1
Hogan <hongju110@126.com>
2022-07-04 11:47:09 +0800

    rate slot

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>home page</h1>
-  <h3>you rate: {{score}}</h3>
-  <Rate v-model="score" />
+  <Rate v-model="score">课程评分</Rate>
+  <Rate v-model="score">
+    <img width="14" src="/favicon.ico">
+  </Rate>
 </template>
 <script setup>
 import { ref } from 'vue';
